49ed4f40f397fac544fda764bde6c7310bae37d4
Jake Kirkbride <jakelegendxiii@gmail.com>
2024-01-18 21:33:09 -0500

    Starting the console app

--- CursedImageTranslator/CursedImageTranslator/Program.cs
+++ CursedImageTranslator/CursedImageTranslator/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Spectre.Console;
+using System.Text;
+
+try
+{
+	// Using UTF8 allows more capabilities for Spectre.Console.
+	Console.OutputEncoding = Encoding.UTF8;
+	Console.InputEncoding = Encoding.UTF8;
+
+	// Display the header
+	AnsiConsole.Write(new FigletText("Cursed Image Translator").Color(Color.Yellow));
+	AnsiConsole.MarkupLine("A dumb little app for understanding images and creating cursed variants.");
+	AnsiConsole.WriteLine();
+
+
+	AnsiConsole.WriteLine("Press any key to exit.");
+	Console.ReadKey();
+
+}
+catch (Exception ex)
+{
+	AnsiConsole.WriteException(ex, ExceptionFormats.ShortenEverything);
+	AnsiConsole.WriteLine();
+	AnsiConsole.WriteLine("Press any key to exit.");
+	Console.ReadKey();
+}
